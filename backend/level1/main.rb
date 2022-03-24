dc02ce60850e9b3471c6eb54ce0c6f991ec301b6
Nuno Costa <nm.cdc@hotmail.com>
2022-03-24 10:18:10 +0000

    done

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -10,12 +10,14 @@ class Car
   end
 
   def rental
-    rentaldays = @start_date - @end_date
-    return rentaldays
+    days = @end_date.mjd - @start_date.mjd
+    return days
   end
 
   def travel
-    total = (rental * @price_per_day) + (@distance * @price_per_km)
+    time = rental * @price_per_day
+    fare = @distance * @price_per_km
+    total = time + fare
     return total
   end
 
@@ -23,15 +25,17 @@ class Car
     file = File.read('./data/input.json')
     data = JSON.parse(file)
     demo1 = Car.new(data['cars'][0]['price_per_day'],data['cars'][0]['price_per_km'],data['rentals'][0]['distance'], Date.parse(data['rentals'][0]['start_date']), Date.parse(data['rentals'][0]['end_date']))
+    demo2 = Car.new(data['cars'][1]['price_per_day'],data['cars'][1]['price_per_km'],data['rentals'][1]['distance'], Date.parse(data['rentals'][1]['start_date']), Date.parse(data['rentals'][1]['end_date']))
+    demo3 = Car.new(data['cars'][2]['price_per_day'],data['cars'][2]['price_per_km'],data['rentals'][2]['distance'], Date.parse(data['rentals'][2]['start_date']), Date.parse(data['rentals'][2]['end_date']))
     print demo1
     tempHash = {
         "rentals": [
           {"id" => 1,
           "price" => demo1.travel },
           {"id" => 2,
-          "price" => demo1.travel},
+          "price" => demo2.travel},
           {"id" => 3,
-          "price" => demo1.travel}
+          "price" => demo3.travel}
         ]
       }
     File.open("./data/expected_output.json" , "w") do |f|
@@ -41,5 +45,5 @@ class Car
     puts jsonified
   end
 a = Car.new(1,1,1,1,1)
-puts a.home
+print a.home
 end
